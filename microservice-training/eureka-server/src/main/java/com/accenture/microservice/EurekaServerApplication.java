cd27ad84af700f1d4249da4cfe53422b94dd6b6c
xiaoguang.wang <xiaoguang.wang@DLC5CG8271X47LW.mshome.net>
2019-06-13 14:01:31 +0800

    init

---
+++ microservice-training/eureka-server/src/main/java/com/accenture/microservice/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.accenture.microservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
